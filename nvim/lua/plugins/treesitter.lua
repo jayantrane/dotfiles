2317eb80f3cc03219dea3ca09341fc8db1b92c31
jayantrane <jayantrane0811@gmail.com>
2026-07-10 19:23:10 +0000

    nvim: Update as per latest

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
 	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	opts = {
 		ensure_installed = {
@@ -28,6 +27,9 @@ return { -- Highlight, edit, and navigate code
 		},
 		indent = { enable = true, disable = { "ruby" } },
 	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 	-- There are additional nvim-treesitter modules that you can use to interact
 	-- with nvim-treesitter. You should go explore a few and see what interests you:
 	--
