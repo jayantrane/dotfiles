d0592a9361e380b46820057b608c89421b484fdf
jayantrane <jayantrane0811@gmail.com>
2026-07-15 16:50:06 +0000

    zsh: syntax highlighter

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,6 +2,7 @@
 source $ANTIGEN_DIR/antigen.zsh
 
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
 antigen theme romkatv/powerlevel10k
 
 antigen apply
